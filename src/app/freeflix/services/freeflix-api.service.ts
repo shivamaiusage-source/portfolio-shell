d1c47fc379f3a90c1c55d6c52d3b38b42f1a7e83
Shivam <shivamsinghitwork@gmail.com>
2026-06-02 16:35:08 +0530

    A-Shivam Connecting freeflix with backend

--- src/app/freeflix/services/freeflix-api.service.ts
+++ src/app/freeflix/services/freeflix-api.service.ts
@@ -1,27 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class FreelixApiService {
 
-  private apiUrl = 'http://localhost:3000/api';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
-  // Videos
   getVideos(search?: string, genre?: string) {
-    let url = `${this.apiUrl}/videos`;
     const params: any = {};
     if (search) params.search = search;
     if (genre) params.genre = genre;
-    return this.http.get<any>(url, { params });
+    return this.http.get<any>(`${this.apiUrl}/videos`, { params });
   }
 
   getVideoById(id: number) {
     return this.http.get<any>(`${this.apiUrl}/videos/${id}`);
   }
 
-  // Watch history
   saveHistory(video_id: number, progress_seconds: number) {
     return this.http.post(`${this.apiUrl}/history`, { video_id, progress_seconds });
   }
@@ -30,7 +28,6 @@ export class FreelixApiService {
     return this.http.get<any>(`${this.apiUrl}/history`);
   }
 
-  // Ratings
   rateVideo(video_id: number, stars: number) {
     return this.http.post(`${this.apiUrl}/ratings`, { video_id, stars });
   }
@@ -39,7 +36,6 @@ export class FreelixApiService {
     return this.http.get<any>(`${this.apiUrl}/ratings/${videoId}`);
   }
 
-  // My List
   addToMyList(video_id: number) {
     return this.http.post(`${this.apiUrl}/mylist/${video_id}`, {});
   }
