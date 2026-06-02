a5c156d1eafb4810d781fa2ed29798986ffdff0b
shivamaiusage-source <shivamaiusage@gmail.com>
2026-06-02 16:55:47 +0530

    A-Shivam Commiting finishing

--- src/app/freeflix/services/auth.service.ts
+++ src/app/freeflix/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable, signal, computed } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { tap } from 'rxjs/operators';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment.prod';
 
 export interface User {
   id: number;
